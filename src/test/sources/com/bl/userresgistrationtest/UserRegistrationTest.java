c0586ab5b1f45374a54561833edec5180ecebfb7
Aman <you@example.com>
2021-10-07 17:34:04 +0530

    Added: Code for Validating The LastName with Conditions

--- src/test/sources/com/bl/userresgistrationtest/UserRegistrationTest.java
+++ src/test/sources/com/bl/userresgistrationtest/UserRegistrationTest.java
@@ -13,45 +13,44 @@ public class UserRegistrationTest {
 	@Test
 	public void Firstname_Shouldbe_True_whenitisproper() {
 		try {
-            boolean result = registration.firstNameValidation("Jaswinder");
-            Assert.assertEquals(true, result);
-        } catch (UserRegistrationProblemException e) {
-            System.out.println(e);
-        }
+			boolean result = registration.firstNameValidation("Jaswinder");
+			Assert.assertEquals(true, result);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 	}
 
 	@Test
 	public void Firstname_Shouldbe_False_whenitisnotproper() {
 		try {
-            boolean result = registration.firstNameValidation("singh");
-        	Assert.assertEquals(result,false);
-        } catch (UserRegistrationProblemException e) {
-            System.out.println(e);
-        }
-	
+			boolean result = registration.firstNameValidation("singh");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
+
 	}
 
 	@Test
 	public void Lastname_Shouldbe_True_whenitisproper() {
 		try {
-            boolean result = registration.lastNameValidation("Sing");
-        	Assert.assertEquals(result,true);
-        } catch (UserRegistrationProblemException e) {
-            System.out.println(e);
-        }
+			boolean result = registration.lastNameValidation("Sing");
+			Assert.assertEquals(result, true);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 
 	}
 
 	@Test
 	public void Lastname_Shouldbe_False_whenitisnotproper() {
 		try {
-            boolean result = registration.lastNameValidation("Don");
-        	Assert.assertEquals(result,false);
-        } catch (UserRegistrationProblemException e) {
-            System.out.println(e);
-        }
+			boolean result = registration.lastNameValidation("Don");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 
 	}
-
 
 }
