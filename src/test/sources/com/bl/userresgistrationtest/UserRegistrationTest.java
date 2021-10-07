f368f2864d12b97056848b6a665bb70a382a5575
Aman <you@example.com>
2021-10-07 18:10:10 +0530

    Added: Code for Validating Password With Minimum 8 Characters

--- src/test/sources/com/bl/userresgistrationtest/UserRegistrationTest.java
+++ src/test/sources/com/bl/userresgistrationtest/UserRegistrationTest.java
@@ -3,69 +3,117 @@ package com.bl.userresgistrationtest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bl.moodanalyser.MoodAnalyser;
 import com.bl.userregistration.UserRegistration;
+import com.bl.userregistration.UserRegistrationProblemException;
 
 public class UserRegistrationTest {
 	UserRegistration registration = new UserRegistration();
-	
+
 	@Test
-    public void Firstname_Shouldbe_True_whenitisproper() {
-     Assert.assertTrue(registration.firstNameValidation("1"));
-    }
-	
+	public void Firstname_Shouldbe_True_whenitisproper() {
+		try {
+			boolean result = registration.firstNameValidation("Jaswinder");
+			Assert.assertEquals(true, result);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
+	}
+
 	@Test
 	public void Firstname_Shouldbe_False_whenitisnotproper() {
-	     Assert.assertFalse(registration.firstNameValidation(""));
+		try {
+			boolean result = registration.firstNameValidation("i");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 
 	}
-	
-	@Test
-    public void Lastname_Shouldbe_True_whenitisproper() {
-        Assert.assertTrue(registration.validLastName("1"));
 
-    }
+	@Test
+	public void Lastname_Shouldbe_True_whenitisproper() {
+		try {
+			boolean result = registration.lastNameValidation("Singh");
+			Assert.assertEquals(result, true);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
+
+	}
 
 	@Test
 	public void Lastname_Shouldbe_False_whenitisnotproper() {
-	     Assert.assertFalse(registration.validLastName(""));
+		try {
+			boolean result = registration.lastNameValidation("Don");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 
 	}
-	
+
 	@Test
 	public void Email_Shouldbe_True_whenitisproper() {
-        Assert.assertTrue(registration.validEmail(""));
+		try {
+			boolean result = registration.validEmail("abc@gmail.com.in");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 
 	}
-	
+
 	@Test
 	public void Email_Shouldbe_False_whenitisnotproper() {
-	     Assert.assertFalse(registration.validEmail("1"));
+		try {
+			boolean result = registration.validEmail("#abc@gmail.com.in");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 
 	}
-	
+
 	@Test
 	public void Mobileno_Shouldbe_True_whenitisproper() {
-        Assert.assertTrue(registration.validPhoneNumber(""));
-
+		try {
+			boolean result = registration.validPhoneNumber("91 984685248");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 	}
-	
+
 	@Test
 	public void Mobileno_Shouldbe_False_whenitisnotproper() {
-        Assert.assertTrue(registration.validPhoneNumber("1"));
-
+		try {
+			boolean result = registration.validPhoneNumber("91 9667914789");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 	}
-	
+
 	@Test
 	public void Passwordtypeone_Shouldbe_True_whenitisproper() {
-        Assert.assertTrue(registration.validPasswordRule1("1"));
+		try {
+			boolean result = registration.validPasswordRule1("password@");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 
 	}
 	
 	@Test
 	public void Passwordtypeone_Shouldbe_False_whenitisproper() {
-        Assert.assertFalse(registration.validPasswordRule1(""));
+		try {
+			boolean result = registration.validPhoneNumber("9qwqw");
+			Assert.assertEquals(result, false);
+		} catch (UserRegistrationProblemException e) {
+			System.out.println(e);
+		}
 
 	}
-
-
 }
